9d6ef89c12cd5b35b82ed518e8b2365bde9aa92d
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 18:05:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+Think about how to determine which value is the lowest. Do you need to compare each value to something as you iterate?
+Think about how to collect or store the correct key. Remember that you need your method to return just this key.
+
 
 def key_for_min_value(name_hash)
 
